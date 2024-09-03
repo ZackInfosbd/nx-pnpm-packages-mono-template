0ce2f0f3142a204abaf383fb123652b097d49ab6
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-09-03 15:21:23 +0200

    test husky

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,6 +3,8 @@ import { sum } from '@certilingu/sample-lib';
 export default function Home() {
   const result = sum(1, 2);
 
+  const result = 1;
+
   return (
     <main className="">
       <div>{result} </div>
